b3503a57bfc81712042f51b9cdc897e8f86b4f6b
deepakkumar9470 <dk397787@gmail.com>
2023-09-20 09:59:52 +0530

    first commit

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'Deepak',
+      last: 'Chouhan',
+    },
+    total: 2795.95,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Suraj',
+      last: 'Kumar',
+    },
+    total: 1195.95,
+    status: 'Processing',
+    method: 'PayPal',
+    date: '23 Minutes ago',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Manti',
+      last: 'Devi',
+    },
+    total: 495.85,
+    status: 'Completed',
+    method: 'Visa',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Raj',
+      last: 'Chouhan',
+    },
+    total: 150.45,
+    status: 'Processing',
+    method: 'MasterCard',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Anshika',
+      last: 'Kumari',
+    },
+    total: 175.25,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Anuj',
+      last: 'Kumar',
+    },
+    total: 1295.75,
+    status: 'Completed',
+    method: 'Check',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Dhiraj',
+      last: 'Kumar',
+    },
+    total: 89.95,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'Pooja',
+      last: 'Kumari',
+    },
+    total: 689.45,
+    status: 'Completed',
+    method: 'Visa',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Siddharth',
+      last: 'Kumar',
+    },
+    total: 14.99,
+    status: 'Completed',
+    method: 'PayPal',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Shilu',
+      last: 'Kumar',
+    },
+    total: 218.99,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '1 Day ago',
+  },
+];
